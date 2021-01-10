cc25bae0c062600b7d6b3950810813f66382eb9f
ep1czoul <ep1czoul@gmail.com>
2021-01-10 21:19:19 +0100

    debugged sequencelock, # als unlock befehl hinzugefügt

--- MatLab/compare.m
+++ MatLab/compare.m
@@ -1,6 +1,6 @@
 function same = compare(array, array_length, password, password_length)
     if(array_length == password_length)
-        same = prod((array(1:array_length)== password(1:password_length)),'all');
+        same = prod((array(1:array_length-1)== password(1:password_length-1)),'all');
     else
         same = 0;
     end
